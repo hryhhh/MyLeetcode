f5e285443ea13639299674871870cae848e8a506
Hry-hhh <13760744+hry-hhh@user.noreply.gitee.com>
2025-07-01 22:09:40 +0800

    fix(283.移动零): 优化移动零的算法

--- 283.移动零.ts
+++ 283.移动零.ts
@@ -9,7 +9,13 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    
+    let lastNonZeroFoundAt = 0;
+    for (let i = 0; i < nums.length;i++){
+        if (nums[i] !== 0) {
+            [nums[lastNonZeroFoundAt] ,nums[i]] = [nums[i],nums[lastNonZeroFoundAt]];
+            lastNonZeroFoundAt++;
+        }
+    }
 };
 // @lc code=end
 
