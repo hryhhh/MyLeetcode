c4538e0ed558dac4db449e25328b9ab5e862dcfa
hruiy <1448585639@qq.com>
2025-10-15 21:25:19 +0800

    feat(typescript): 移除未使用的TreeNode注释并实现链表合并功能
    移除了102题中未使用的TreeNode类注释代码，并在21题中添加了ListNode类定义以支持
    链表合并逻辑的实现。更新了相关函数以符合题目要求。

--- 102.二叉树的层序遍历.ts
+++ 102.二叉树的层序遍历.ts
@@ -18,17 +18,7 @@
  *     }
  * }
  */
-// class TreeNode {
-//   val: number;
-//   left: TreeNode | null;
-//   right: TreeNode | null;
 
-//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//     this.val = val === undefined ? 0 : val;
-//     this.left = left === undefined ? null : left;
-//     this.right = right === undefined ? null : right;
-//   }
-// }
 function levelOrder(root: TreeNode | null): number[][] {
     const res: number[][] = [];
     if (!root) return res;
--- 21.合并两个有序链表.ts
+++ 21.合并两个有序链表.ts
@@ -7,16 +7,15 @@
 // @lc code=start
 /**
  * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
  */
-
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
     const dummyHead = new ListNode(-1);
     let curNode: ListNode | null = dummyHead
